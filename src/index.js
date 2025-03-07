f280a740d315b856634f2ab7abc2c1ca0dfb45b2
Deman <deman2805@gmail.com>
2025-03-07 13:35:29 +0200

    end hw

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-const message = "Hello world";
+import { setupServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 
-console.log(message);
+const bootstrap = async () => {
+  try {
+    await initMongoConnection();
+    setupServer();
+  } catch (e) {
+    console.log('Error during setup', e);
+  }
+};
+
+bootstrap();
