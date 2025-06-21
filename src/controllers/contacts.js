0d7c1fa5de91eb41118c15b4f57379ba639ddba6
Tap0kk <deman2805@gmail.com>
2025-06-22 00:18:18 +0300

    add pagination

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -22,7 +22,7 @@ export const getContactsController = async (req, res, next) => {
 
     const userId = req.user._id;
 
-    const contacts = await getAllContacts({
+    const {contacts, total} = await getAllContacts({
       page,
       perPage,
       sortBy,
@@ -31,15 +31,18 @@ export const getContactsController = async (req, res, next) => {
       userId,
     });
 
+    const totalPages = Math.ceil(total / perPage);
+
     res.json({
       status: 200,
       message: 'Successfully found contacts!',
       data: {
         contacts,
         total,
-      page: Number(page),
-      limit: Number(limit),
-            }
+        page: Number(page),
+        perPage: Number(perPage),
+        totalPages,
+      },
     });
   } catch (err) {
     next(err);
@@ -64,12 +67,8 @@ export const getContactByIdController = async (req, res, next) => {
     res.status(200).json({
       status: 200,
       message: `Successfully found contact with id ${contactId}!`,
-      data: {
-    contacts,
-    total,
-    page,
-    limit
-  },
+      data: 
+    contact,
     });
   } catch (err) {
     next(err);
